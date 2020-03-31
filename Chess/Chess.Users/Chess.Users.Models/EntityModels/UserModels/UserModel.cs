1d663addd9fdb25f7a6f153ef38f8f80c47ad4c8
jivko.milev <jivko.milev@primeholding.com>
2020-03-31 20:17:47 +0300

    Hash password upon user creation

--- Chess/Chess.Users/Chess.Users.Models/EntityModels/UserModels/UserModel.cs
+++ Chess/Chess.Users/Chess.Users.Models/EntityModels/UserModels/UserModel.cs
@@ -5,7 +5,6 @@ namespace Chess.Users.Models.EntityModels.UserModels
 {
     public class UserModel : BaseModel, IUserModel
     {
-        public Guid Id { get; set; }
         public string Username { get; set; }
         public string Password { get; set; }
         public string Email { get; set; }
--- Chess/Chess.Users/Chess.Users.Services/EntityServices/UserService.cs
+++ Chess/Chess.Users/Chess.Users.Services/EntityServices/UserService.cs
@@ -4,6 +4,7 @@ using Chess.Users.DataAccess.Repositories.EntityRepositories;
 using Chess.Users.DataAccess.Repositories.Interfaces;
 using Chess.Users.Models.EntityModels.UserModels;
 using Chess.Users.Services.EntityServices.Interfaces;
+using Chess.Users.Utilities;
 using Chess.Users.Utilities.Interfaces;
 
 namespace Chess.Users.Services.EntityServices
@@ -14,5 +15,17 @@ namespace Chess.Users.Services.EntityServices
             IDateTimeProvider dateTimeProvider, 
             IMapper mapper) 
             : base(unitOfWork, dateTimeProvider, mapper) { }
+
+        protected override void OnBeforeUpdate(User entity)
+        {
+            base.OnBeforeUpdate(entity);
+        }
+
+        protected override void OnBeforeInsert(User entity)
+        {
+            base.OnBeforeInsert(entity);
+
+            entity.Password = PasswordHasher.HashPassword(entity.Password);
+        }
     }
 }
