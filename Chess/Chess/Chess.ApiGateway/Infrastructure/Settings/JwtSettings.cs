079b4d1045e3c126d42c7dfd73a21c54dda0d6e4
jivko.milev <jivko.milev@primeholding.com>
2020-04-25 18:46:40 +0300

    Remove JWT ExpirationDate form Api Gateway
    Related work items: #5

--- Chess/Chess/Chess.ApiGateway/Infrastructure/Settings/JwtSettings.cs
+++ Chess/Chess/Chess.ApiGateway/Infrastructure/Settings/JwtSettings.cs
@@ -8,7 +8,6 @@ namespace Chess.ApiGateway.Api.Infrastructure.Settings
     public class JwtSettings
     {
         public string Key { get; set; }
-        public string ExpirationInMinutes { get; set; }
         public string Issuer { get; set; }
         public string[] Audiences { get; set; }
     }
