1df33c13742e141253b23d068bf47c4d5387b563
Givko <milevjivko@gmail.com>
2020-05-31 00:22:29 +0300

    Add models

---
+++ Chess/Chess/Chess.GameEngine.Models/GameModels/ChessBoardModels/Position.cs
@@ -0,0 +1,8 @@
+﻿namespace Chess.GameEngine.Models.GameModels.ChessBoardModels
+{
+    public class Position
+    {
+        public byte X { get; set; }
+        public byte Y { get; set; }
+    }
+}
---
+++ Chess/Chess/Chess.GameEngine.Models/PlayerInfoModel.cs
@@ -0,0 +1,8 @@
+﻿namespace Chess.GameEngine.Models
+{
+    public class PlayerInfoModel
+    {
+        public string Username { get; set; }
+        public int Points { get; set; }
+    }
+}
